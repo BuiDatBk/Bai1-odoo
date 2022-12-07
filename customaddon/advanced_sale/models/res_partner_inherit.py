74a60a8bc2e12c9a88224b04e07d2bf0a979af0c
BuiDatBk <buivandaty2k@gmail.com>
2022-12-07 09:06:13 +0700

    Bai1.3

--- customaddon/advanced_sale/models/res_partner_inherit.py
+++ customaddon/advanced_sale/models/res_partner_inherit.py
@@ -1,10 +1,34 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
 
 class ResPartnerInherit(models.Model):
     _inherit = "res.partner"
     _description = ""
 
-    customer_discount_code = fields.Text('Discount Code')
+    customer_discount_code = fields.Text('Discount Code', groups="advanced_sale.group_advanced_sale")
+    discount_percentage = fields.Integer("Discount Percentage", compute="_discount_percentage")
+
+    def _is_valid_discount_code(self):
+        self.ensure_one()
+        char_list = self.customer_discount_code.split("_")
+        if len(char_list) == 2 and char_list[0] == "VIP":
+            try:
+                discount_amount = int(char_list[1])
+                if 1 < discount_amount < 100:
+                    return True
+                else:
+                    return False
+            except ValueError:
+                return False
+
+    @api.depends("customer_discount_code")
+    def _discount_percentage(self):
+        for rec in self:
+            if rec._is_valid_discount_code():
+                rec.discount_percentage = int(rec.customer_discount_code.split("_")[1])
+            else:
+                rec.discount_percentage = False
---
+++ customaddon/advanced_sale/models/sale_order_inherit.py
@@ -0,0 +1,28 @@
+from odoo import models, fields, api
+
+
+class SaleOrderInherit(models.Model):
+    _inherit = "sale.order"
+    _description = ""
+
+    sale_order_discount_estimated = fields.Float(string="Discount Total",
+                                                 compute="_compute_discount_total"
+                                                 )
+
+    @api.depends('partner_id.discount_percentage')
+    def _compute_discount_total(self):
+        for customer in self:
+            if customer.partner_id.discount_percentage:
+                customer.sale_order_discount_estimated = \
+                    (customer.amount_untaxed * customer.partner_id.discount_percentage) / 100
+            else:
+                customer.sale_order_discount_estimated = 0
+
+    def _amount_all(self):
+        super(self._amount_all())
+        amount_total = 0
+        for customer in self:
+            amount_total = customer.amount_total - customer.sale_order_discount_estimated
+            customer.update({
+                'amount_total': amount_total
+            })
